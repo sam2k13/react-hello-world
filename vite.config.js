ce89331ac30abe5ec3ce393088011b297d1d24e4
Sam Kleist <samkleist@cloudfitsoftware.com>
2025-11-23 16:31:54 -0500

    Add allowed host for Azure Container Apps

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 8080,
-    strictPort: true
+    strictPort: true,
+    allowedHosts: [
+      'codegen-playground-app.orangefield-9820aa17.eastus.azurecontainerapps.io'
+    ]
   }
 })
