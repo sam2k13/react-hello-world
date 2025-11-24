139977468cacb9d68df3b31db0a868499644ae33
Sam Kleist <samkleist@cloudfitsoftware.com>
2025-11-23 21:21:40 -0500

    push up vite proxy

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,16 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 5173,
+    port: 8080,
     strictPort: true,
     allowedHosts: [
       'codegen-playground-app.orangefield-9820aa17.eastus.azurecontainerapps.io'
-    ]
+    ],
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true
+      }
+    }
   }
 })
